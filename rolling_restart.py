b827dc01fe9f5a06fca2af46835cc1c49ab20a91
janik <hartger.janik@gmail.com>
2024-11-21 18:22:38 +0100

    better logging rolling_restart.py

--- rolling_restart.py
+++ rolling_restart.py
@@ -1,14 +1,20 @@
 import os
 import time
-
+import logging
 import requests
 import urllib3
 from dotenv import load_dotenv
-
-# load environment variables from .env_example file in same directory
 from mail_notification import mail_notification
 
+# Load environment variables from .env_example file in the same directory
 load_dotenv()
+
+# Setup logging with timestamps
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 # UniFi controller details
 controller_ip = os.getenv("CONTROLLER_IP")
@@ -16,6 +22,9 @@ controller_port = os.getenv("CONTROLLER_PORT")
 controller_username = os.getenv("CONTROLLER_USERNAME")
 controller_password = os.getenv("CONTROLLER_PASSWORD")
 site_name = os.getenv("SITE_NAME")
+
+# Subject for mail
+subject = os.getenv("SUBJECT")
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -30,7 +39,7 @@ login_data = {"username": controller_username, "password": controller_password}
 login_response = requests.post(login_url, json=login_data, verify=True)
 
 if login_response.status_code == 200:
-    print("Login successful.")
+    logging.info("Login successful. Starting Rolling Restart.")
     cookies = login_response.cookies
 
     # Retrieve the list of access points
@@ -46,13 +55,13 @@ if login_response.status_code == 200:
                 restart_response = requests.post(restart_ap_url, json={"mac": ap["mac"]}, cookies=cookies, verify=False)
 
                 if restart_response.status_code == 200:
-                    print(f"Access point {ap['mac']} restart command issued.")
+                    logging.info(f"Access point {ap['mac']} restart command issued.")
 
                     # Wait for the access point to come back online or until 5 minutes have passed
                     timeout = time.time() + 300  # 5 minutes timeout
                     while True:
                         if time.time() > timeout:
-                            print(f"Timeout occurred for access point {ap['mac']}. Moving to the next AP.")
+                            logging.warning(f"Timeout occurred for access point {ap['mac']}. Moving to the next AP.")
                             break
 
                         # Check the status of the access point
@@ -62,27 +71,22 @@ if login_response.status_code == 200:
                             for ap_status in ap_status_list:
                                 if ap_status["_id"] == ap["_id"]:
                                     if ap_status["state"] == 1:  # Access point is online
-                                        print(f"Access point {ap['mac']} is back online.")
+                                        logging.info(f"Access point {ap['mac']} is back online.")
                                         break
                             else:
                                 time.sleep(5)  # Wait for 5 seconds before checking the status again
-                                print("Access point is still restarting")
                                 continue
                         else:
-                            print(f"Failed to retrieve the status of access point {ap['mac']}. Moving to the next AP. "
-                                  f"Status Code: ", ap_status_response.status_code)
+                            logging.error(f"Failed to retrieve the status of access point {ap['mac']}. Moving to the next AP. "
+                                          f"Status Code: {ap_status_response.status_code}")
                             break
 
                         break  # Break out of the while loop if the access point is online
                 else:
-                    print(f"Failed to issue restart command for access point {ap['mac']}. Status Code: ",
-                          restart_response.status_code)
-
-        print("Rolling restart of access points completed.")
+                    logging.error(f"Failed to issue restart command for access point {ap['mac']}. Status Code: {restart_response.status_code}")
 
         # Create a new list to store ap's items
         new_ap_list = []
-
 
         for ap in ap_list:
             if "name" not in ap:  # devices without alias don't have a 'name' field
@@ -107,13 +111,13 @@ if login_response.status_code == 200:
             mail_notification(subject, body)
 
     else:
-        print("Failed to retrieve the list of access points. Status Code: ", ap_response.status_code)
+        logging.error(f"Failed to retrieve the list of access points. Status Code: {ap_response.status_code}")
 
     # Logout from UniFi controller
     logout_response = requests.post(logout_url, cookies=cookies, verify=False)
     if logout_response.status_code == 200:
-        print("Logout successful.")
+        logging.info("Logout successful. Rolling Restart completed.")
     else:
-        print("Failed to logout.")
+        logging.error("Failed to logout.")
 else:
-    print("Login failed. Status Code: ", login_response.status_code)
+    logging.error(f"Login failed. Status Code: {login_response.status_code}")
